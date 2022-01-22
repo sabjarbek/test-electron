042309f6651d216c98058657362b4da4d1ea4aa5
sanjarbek azizov <azizov@gmail.com>
2022-01-23 00:19:13 +0500

    i need set ajax

--- php/ajax.php
+++ php/ajax.php
@@ -1,1 +1,27 @@
-echo "hello everyone"
+
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "mysql", "", "message_table");
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+$first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
+$last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO messages (name, message, status, sex) VALUES ('$first_name', '$last_name', 'sanjar', 'litr')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
+?>
